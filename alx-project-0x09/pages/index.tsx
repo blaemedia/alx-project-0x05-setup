50d01deb55be39417c77811979068037672e4662
Emmanuel <blaemedia26@gmail.com>
2025-12-05 02:59:56 +0100

    Generate Image based on user prompt

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -1,6 +1,10 @@
 import ImageCard from "@/components/common/ImageCard";
 import { ImageProps } from "@/interfaces";
-import React, { useState } from "react";
+import { useState } from "react";
+
+
+
+
 
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
@@ -11,29 +15,11 @@ const Home: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
 
+  
   const handleGenerateImage = async () => {
-    setIsLoading(true);
-    const resp = await fetch('/api/generate-image', {
-      method: 'POST',
-      body: JSON.stringify({
-        prompt
-      }),
-      headers: {
-        'Content-type': 'application/json'
-      }
-    })
-
-
-    if (!resp.ok) {
-      setIsLoading(false)
-      return;
-    }
-
-    const data = await resp.json()
-    setIsLoading(false)
-    setImageUrl(data?.message);
-    setGeneratedImages((prev) => [...prev, { imageUrl: data?.message, prompt }])
-  };
+      console.log("Generating Image")
+      console.log(process.env.NEXT_PUBLIC_GPT_API_KEY)
+    };
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
@@ -55,36 +41,15 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
-            {
+            {/* {
               isLoading ? "Loading..." : "Generate Image"
-            }
+            } */}
+            Generate Image
           </button>
         </div>
 
         {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
       </div>
-      {
-        generatedImages.length ? (
-          <div className="">
-            <h3 className="text-xl text-center mb-4">Generated Images</h3>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 border max-w-full md:max-w-[1100px] p-2 overflow-y-scroll h-96">
-              {generatedImages?.map(
-                ({ imageUrl, prompt }: ImageProps, index) => (
-                  <ImageCard
-                    action={() => setImageUrl(imageUrl)}
-                    imageUrl={imageUrl}
-                    prompt={prompt}
-                    key={index}
-                    width="w-full"
-                    height="h-40"
-                  />
-                )
-              )}
-            </div>
-          </div>
-
-        ) : ""
-      }
     </div>
   );
 };
